70c04b4f3d2794136a5a5ac088b88cb4fd609b77
TheTabular <thetabular@gmail.com>
2024-04-16 14:13:46 -0500

    Update frontend URL

--- src/App.js
+++ src/App.js
@@ -59,10 +59,10 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://strainer-env.eba-7qvtienz.us-east-1.elasticbeanstalk.com/recommend', {
+      const response = await axios.post('https://api.strainer.wiki/recommend', {
         effects: selectedEffects,
         flavors: selectedFlavors,
-        types: selectedTypes
+        types: selectedTypes,
       });
       setTopStrains(response.data.top_strains.slice(0, 5)); // Get the top 5 strains
       setShowTopStrains(true);
